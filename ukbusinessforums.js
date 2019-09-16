35de2f76b9b79f35baafcc489016c2dbcb68bc59
seongjaemoon <seongjae.moon.92@gmail.com>
2019-09-16 16:17:18 +0100

    uk business forum config

---
+++ ukbusinessforums.js
@@ -0,0 +1,607 @@
+var streamampConfig = {
+    a9Enabled: false,
+    bidTimeout: 1.2,
+    pbjsPriceGranularity: 'high',
+    hasRefreshBids: false,
+    refreshBidsTimeout: 90,
+    hasCollapsedEmptyDivs: true,
+    publisher_id: 'SiftMedia/UKBusinessForum',
+    token: 'JJd43srQfu97oyw7PLf3LSfxvIZj6CgwdzZh0EAbVd3JslCNYLJpxzOI5ohYl5xH',
+    currency: {
+        enabled: true,
+        value: 'GBP'
+    },
+    adUnits: [
+        {
+            code: 'ukbf-mpu',
+            path: '/4394518/ukbf-mpu',
+            mediaTypes: {
+                banner: {
+                    sizes: [
+                        [
+                            300,
+                            600
+                        ],
+                        [
+                            300,
+                            250
+                        ]
+                    ]
+                }
+            },
+            isSticky: false,
+            safeFrame: true,
+            outOfPage: false,
+            bids: [
+                {
+                    bidder: 'criteo',
+                    labelAny: [
+                        '971 - 9999'
+                    ],
+                    params: {
+                        zoneId: 1382492,
+                        publisherSubId: 'ukbf_mpu_300x600'
+                    }
+                },
+                {
+                    bidder: 'criteo',
+                    labelAny: [
+                        '971 - 9999',
+                        '728 - 970',
+                        '0 - 720'
+                    ],
+                    params: {
+                        zoneId: 1382491,
+                        publisherSubId: 'ukbf_mpu_300x250'
+                    }
+                },
+                {
+                    bidder: 'emx_digital',
+                    labelAny: [
+                        '971 - 9999'
+                    ],
+                    params: {
+                        tagid: '70983'
+                    }
+                },
+                {
+                    bidder: 'emx_digital',
+                    labelAny: [
+                        '971 - 9999',
+                        '728 - 970',
+                        '0 - 720'
+                    ],
+                    params: {
+                        tagid: '70984'
+                    }
+                },
+                {
+                    bidder: 'improvedigital',
+                    labelAny: [
+                        '971 - 9999'
+                    ],
+                    params: {
+                        placementId: 22039666
+                    }
+                },
+                {
+                    bidder: 'improvedigital',
+                    labelAny: [
+                        '971 - 9999',
+                        '728 - 970',
+                        '0 - 720'
+                    ],
+                    params: {
+                        placementId: 22039666
+                    }
+                },
+                {
+                    bidder: 'openx',
+                    labelAny: [
+                        '971 - 9999'
+                    ],
+                    params: {
+                        unit: '540778444',
+                        delDomain: 'streamamp-d.openx.net'
+                    }
+                },
+                {
+                    bidder: 'openx',
+                    labelAny: [
+                        '971 - 9999',
+                        '728 - 970',
+                        '0 - 720'
+                    ],
+                    params: {
+                        unit: '540778444',
+                        delDomain: 'streamamp-d.openx.net'
+                    }
+                },
+                {
+                    bidder: 'pubmatic',
+                    labelAny: [
+                        '971 - 9999'
+                    ],
+                    params: {
+                        publisherId: '2237787',
+                        adSlot: 'Sift-ukbf_mpu_300x600'
+                    }
+                },
+                {
+                    bidder: 'pubmatic',
+                    labelAny: [
+                        '971 - 9999',
+                        '728 - 970',
+                        '0 - 720'
+                    ],
+                    params: {
+                        publisherId: '2237788',
+                        adSlot: 'Sift-ukbf_mpu_300x250'
+                    }
+                },
+                {
+                    bidder: 'streamamp',
+                    labelAny: [
+                        '971 - 9999'
+                    ],
+                    params: {
+                        placementId: 16175433
+                    }
+                },
+                {
+                    bidder: 'streamamp',
+                    labelAny: [
+                        '971 - 9999',
+                        '728 - 970',
+                        '0 - 720'
+                    ],
+                    params: {
+                        placementId: 16175510
+                    }
+                }
+            ]
+        },
+        {
+            code: 'ukbf-mpu-2',
+            path: '/4394518/ukbf-mpu-2',
+            mediaTypes: {
+                banner: {
+                    sizes: [
+                        [
+                            300,
+                            600
+                        ],
+                        [
+                            300,
+                            250
+                        ]
+                    ]
+                }
+            },
+            isSticky: false,
+            safeFrame: true,
+            outOfPage: false,
+            bids: [
+                {
+                    bidder: 'criteo',
+                    labelAny: [
+                        '971 - 9999'
+                    ],
+                    params: {
+                        zoneId: 1382492,
+                        publisherSubId: 'ukbf_mpu_2_300x600'
+                    }
+                },
+                {
+                    bidder: 'criteo',
+                    labelAny: [
+                        '971 - 9999',
+                        '728 - 970',
+                        '0 - 720'
+                    ],
+                    params: {
+                        zoneId: 1382491,
+                        publisherSubId: 'ukbf_mpu_2_300x250'
+                    }
+                },
+                {
+                    bidder: 'emx_digital',
+                    labelAny: [
+                        '971 - 9999'
+                    ],
+                    params: {
+                        tagid: '70985'
+                    }
+                },
+                {
+                    bidder: 'emx_digital',
+                    labelAny: [
+                        '971 - 9999',
+                        '728 - 970',
+                        '0 - 720'
+                    ],
+                    params: {
+                        tagid: '70986'
+                    }
+                },
+                {
+                    bidder: 'improvedigital',
+                    labelAny: [
+                        '971 - 9999'
+                    ],
+                    params: {
+                        placementId: 22039670
+                    }
+                },
+                {
+                    bidder: 'improvedigital',
+                    labelAny: [
+                        '971 - 9999',
+                        '728 - 970',
+                        '0 - 720'
+                    ],
+                    params: {
+                        placementId: 22039670
+                    }
+                },
+                {
+                    bidder: 'openx',
+                    labelAny: [
+                        '971 - 9999'
+                    ],
+                    params: {
+                        unit: '540778445',
+                        delDomain: 'streamamp-d.openx.net'
+                    }
+                },
+                {
+                    bidder: 'openx',
+                    labelAny: [
+                        '971 - 9999',
+                        '728 - 970',
+                        '0 - 720'
+                    ],
+                    params: {
+                        unit: '540778445',
+                        delDomain: 'streamamp-d.openx.net'
+                    }
+                },
+                {
+                    bidder: 'pubmatic',
+                    labelAny: [
+                        '971 - 9999'
+                    ],
+                    params: {
+                        publisherId: '2237789',
+                        adSlot: 'Sift-ukbf_mpu_2_300x600'
+                    }
+                },
+                {
+                    bidder: 'pubmatic',
+                    labelAny: [
+                        '971 - 9999',
+                        '728 - 970',
+                        '0 - 720'
+                    ],
+                    params: {
+                        publisherId: '2237790',
+                        adSlot: 'Sift-ukbf_mpu_2_300x250'
+                    }
+                },
+                {
+                    bidder: 'streamamp',
+                    labelAny: [
+                        '971 - 9999'
+                    ],
+                    params: {
+                        placementId: 16175511
+                    }
+                },
+                {
+                    bidder: 'streamamp',
+                    labelAny: [
+                        '971 - 9999',
+                        '728 - 970',
+                        '0 - 720'
+                    ],
+                    params: {
+                        placementId: 16175513
+                    }
+                }
+            ]
+        },
+        {
+            code: 'ukbf-top',
+            path: '/4394518/ukbf-top',
+            mediaTypes: {
+                banner: {
+                    sizes: [
+                        [
+                            970,
+                            250
+                        ],
+                        [
+                            728,
+                            90
+                        ],
+                        [
+                            320,
+                            50
+                        ]
+                    ]
+                }
+            },
+            isSticky: false,
+            safeFrame: true,
+            outOfPage: false,
+            bids: [
+                {
+                    bidder: 'criteo',
+                    labelAny: [
+                        '971 - 9999'
+                    ],
+                    params: {
+                        zoneId: 1382488,
+                        publisherSubId: 'ukbf_top_970x250'
+                    }
+                },
+                {
+                    bidder: 'criteo',
+                    labelAny: [
+                        '971 - 9999',
+                        '728 - 970'
+                    ],
+                    params: {
+                        zoneId: 1382489,
+                        publisherSubId: 'ukbf_top_728x90'
+                    }
+                },
+                {
+                    bidder: 'criteo',
+                    labelAny: [
+                        '0 - 720'
+                    ],
+                    params: {
+                        zoneId: 1382490,
+                        publisherSubId: 'ukbf_top_320x50'
+                    }
+                },
+                {
+                    bidder: 'emx_digital',
+                    labelAny: [
+                        '971 - 9999'
+                    ],
+                    params: {
+                        tagid: '70987'
+                    }
+                },
+                {
+                    bidder: 'emx_digital',
+                    labelAny: [
+                        '971 - 9999',
+                        '728 - 970'
+                    ],
+                    params: {
+                        tagid: '70988'
+                    }
+                },
+                {
+                    bidder: 'emx_digital',
+                    labelAny: [
+                        '0 - 720'
+                    ],
+                    params: {
+                        tagid: '70989'
+                    }
+                },
+                {
+                    bidder: 'improvedigital',
+                    labelAny: [
+                        '971 - 9999'
+                    ],
+                    params: {
+                        placementId: 22039671
+                    }
+                },
+                {
+                    bidder: 'improvedigital',
+                    labelAny: [
+                        '971 - 9999',
+                        '728 - 970'
+                    ],
+                    params: {
+                        placementId: 22039671
+                    }
+                },
+                {
+                    bidder: 'improvedigital',
+                    labelAny: [
+                        '0 - 720'
+                    ],
+                    params: {
+                        placementId: 22039671
+                    }
+                },
+                {
+                    bidder: 'openx',
+                    labelAny: [
+                        '971 - 9999'
+                    ],
+                    params: {
+                        unit: '540778452',
+                        delDomain: 'streamamp-d.openx.net'
+                    }
+                },
+                {
+                    bidder: 'openx',
+                    labelAny: [
+                        '971 - 9999',
+                        '728 - 970'
+                    ],
+                    params: {
+                        unit: '540778452',
+                        delDomain: 'streamamp-d.openx.net'
+                    }
+                },
+                {
+                    bidder: 'openx',
+                    labelAny: [
+                        '0 - 720'
+                    ],
+                    params: {
+                        unit: '540778452',
+                        delDomain: 'streamamp-d.openx.net'
+                    }
+                },
+                {
+                    bidder: 'pubmatic',
+                    labelAny: [
+                        '971 - 9999'
+                    ],
+                    params: {
+                        publisherId: '2237791',
+                        adSlot: 'Sift-ukbf_top_970x250'
+                    }
+                },
+                {
+                    bidder: 'pubmatic',
+                    labelAny: [
+                        '971 - 9999',
+                        '728 - 970'
+                    ],
+                    params: {
+                        publisherId: '2237792',
+                        adSlot: 'Sift-ukbf_top_728x90'
+                    }
+                },
+                {
+                    bidder: 'pubmatic',
+                    labelAny: [
+                        '0 - 720'
+                    ],
+                    params: {
+                        publisherId: '2237793',
+                        adSlot: 'Sift-ukbf_top_320x50'
+                    }
+                },
+                {
+                    bidder: 'streamamp',
+                    labelAny: [
+                        '971 - 9999'
+                    ],
+                    params: {
+                        placementId: 16175518
+                    }
+                },
+                {
+                    bidder: 'streamamp',
+                    labelAny: [
+                        '971 - 9999',
+                        '728 - 970'
+                    ],
+                    params: {
+                        placementId: 16175519
+                    }
+                },
+                {
+                    bidder: 'streamamp',
+                    labelAny: [
+                        '0 - 720'
+                    ],
+                    params: {
+                        placementId: 16175516
+                    }
+                }
+            ]
+        }
+    ],
+    cmp: {
+        isEnabled: true,
+        config: {
+            'UI Layout': 'popup',
+            Language: 'en',
+            'No Option': false,
+            'Non-Consent Display Frequency': 7,
+            'Publisher Purpose IDs': [],
+            'Initial Screen Body Text Option': 1,
+            'Initial Screen Accept Button Text': 'I agree',
+            'Initial Screen Reject Button Text': 'I do not accept',
+            'Purpose Screen Body Text': 'You can set your consent preferences and determine how you want your data to be used based on the purposes below. You may set your preferences for us independently from those of third-party partners. Each purpose has a description so that you know how we and partners use your data.',
+            'Vendor Screen Body Text': 'You can set consent preferences for each individual third-party company below. Expand each company list item to see what purposes they use data for to help make your choices. In some cases, companies may disclose that they use your data without asking for your consent, based on their legitimate interests. You can click on their privacy policies for more information and to opt out.',
+            'Vendor Screen Accept All Button Text': 'Accept all',
+            'Vendor Screen Reject All Button Text': 'Reject all',
+            'Publisher Name': 'UK Business Forums',
+            'Publisher Logo': 'https://static.amp.services/logos/siftlogo.png'
+        },
+        hasCustomStyles: false,
+        styles: {
+            ui: {},
+            link: {},
+            primaryButton: {},
+            primaryButtonHover: {},
+            isSecondaryButtonHidden: false,
+            secondaryButton: {},
+            secondaryButtonHover: {},
+            tableHeader: {},
+            tableRow: {},
+            toggleOn: {},
+            toggleOff: {},
+            additionalStyles: ''
+        }
+    },
+    breakpoints: [
+        {
+            label: '971 - 9999',
+            minWidth: 971,
+            maxWidth: 9999,
+            sizesSupported: [
+                [
+                    300,
+                    600
+                ],
+                [
+                    300,
+                    250
+                ],
+                [
+                    970,
+                    250
+                ],
+                [
+                    728,
+                    90
+                ]
+            ]
+        },
+        {
+            label: '728 - 970',
+            minWidth: 728,
+            maxWidth: 970,
+            sizesSupported: [
+                [
+                    300,
+                    250
+                ],
+                [
+                    728,
+                    90
+                ]
+            ]
+        },
+        {
+            label: '0 - 720',
+            minWidth: 0,
+            maxWidth: 720,
+            sizesSupported: [
+                [
+                    300,
+                    250
+                ],
+                [
+                    320,
+                    50
+                ]
+            ]
+        }
+    ]
+}
