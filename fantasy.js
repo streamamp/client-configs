1773e12ae6b5735acd0191b2456d0d6926a342d1
seongjaemoon <seongjae.moon.92@gmail.com>
2019-09-18 16:20:16 +0100

    farrelly atkinson ltd fantasy road initial script

---
+++ fantasy.js
@@ -0,0 +1,444 @@
+var streamampConfig = {
+    a9Enabled: false,
+    bidTimeout: 1.2,
+    pbjsPriceGranularity: 'high',
+    hasRefreshBids: false,
+    refreshBidsTimeout: 90,
+    hasCollapsedEmptyDivs: true,
+    publisher_id: 'FarrellyAtkinsonLtd/fantasyroadcc',
+    token: '3a6rREoYyE5XEvQFb59RalGbWyDNKTYENugQt4ExZ8HuUHRfibYwUhRVws5Jp9rZ',
+    currency: {
+        enabled: true,
+        value: 'GBP'
+    },
+    adUnits: [
+        {
+            code: 'SA-Unit1',
+            path: '/269479563/Fantasy.road.cc/SA-Unit1',
+            mediaTypes: {
+                banner: {
+                    sizes: [
+                        [
+                            970,
+                            250
+                        ],
+                        [
+                            728,
+                            90
+                        ]
+                    ]
+                }
+            },
+            isSticky: false,
+            safeFrame: true,
+            outOfPage: false,
+            bids: [
+                {
+                    bidder: 'criteo',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        zoneId: 1382489,
+                        publisherSubId: 'FA_Fantasy_Unit1_728x90'
+                    }
+                },
+                {
+                    bidder: 'districtmDMX',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        dmxid: 375139,
+                        memberid: 100615
+                    }
+                },
+                {
+                    bidder: 'emx_digital',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        tagid: '41046'
+                    }
+                },
+                {
+                    bidder: 'improvedigital',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        placementId: 1204087
+                    }
+                },
+                {
+                    bidder: 'ix',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        siteId: '261315',
+                        size: [
+                            970,
+                            250
+                        ]
+                    }
+                },
+                {
+                    bidder: 'ix',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        siteId: '305794',
+                        size: [
+                            728,
+                            90
+                        ]
+                    }
+                },
+                {
+                    bidder: 'openx',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        unit: '539929980',
+                        delDomain: 'streamamp-d.openx.net'
+                    }
+                },
+                {
+                    bidder: 'streamamp',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        placementId: 13075144
+                    }
+                },
+                {
+                    bidder: 'streamamp',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        placementId: 13075163
+                    }
+                }
+            ]
+        },
+        {
+            code: 'SA-Unit2',
+            path: '/269479563/Fantasy.road.cc/SA-Unit2',
+            mediaTypes: {
+                banner: {
+                    sizes: [
+                        [
+                            300,
+                            250
+                        ],
+                        [
+                            300,
+                            600
+                        ]
+                    ]
+                }
+            },
+            isSticky: false,
+            safeFrame: true,
+            outOfPage: false,
+            bids: [
+                {
+                    bidder: 'criteo',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        zoneId: 1382491,
+                        publisherSubId: 'FA_Fantasy_Unit2_300x250'
+                    }
+                },
+                {
+                    bidder: 'districtmDMX',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        dmxid: 375140,
+                        memberid: 100615
+                    }
+                },
+                {
+                    bidder: 'emx_digital',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        tagid: '41047'
+                    }
+                },
+                {
+                    bidder: 'improvedigital',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        placementId: 1204088
+                    }
+                },
+                {
+                    bidder: 'ix',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        siteId: '305801',
+                        size: [
+                            300,
+                            600
+                        ]
+                    }
+                },
+                {
+                    bidder: 'ix',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        siteId: '305799',
+                        size: [
+                            300,
+                            250
+                        ]
+                    }
+                },
+                {
+                    bidder: 'openx',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        unit: '539929985',
+                        delDomain: 'streamamp-d.openx.net'
+                    }
+                },
+                {
+                    bidder: 'streamamp',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        placementId: 13075166
+                    }
+                },
+                {
+                    bidder: 'streamamp',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        placementId: 13075165
+                    }
+                }
+            ]
+        },
+        {
+            code: 'SA-Unit3',
+            path: '/269479563/Fantasy.road.cc/SA-Unit3',
+            mediaTypes: {
+                banner: {
+                    sizes: [
+                        [
+                            300,
+                            250
+                        ],
+                        [
+                            300,
+                            600
+                        ]
+                    ]
+                }
+            },
+            isSticky: false,
+            safeFrame: true,
+            outOfPage: false,
+            bids: [
+                {
+                    bidder: 'criteo',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        zoneId: 1382491,
+                        publisherSubId: 'FA_Fantasy_Unit3_300x250'
+                    }
+                },
+                {
+                    bidder: 'districtmDMX',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        dmxid: 375141,
+                        memberid: 100615
+                    }
+                },
+                {
+                    bidder: 'emx_digital',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        tagid: '41048'
+                    }
+                },
+                {
+                    bidder: 'improvedigital',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        placementId: 1204089
+                    }
+                },
+                {
+                    bidder: 'ix',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        siteId: '305802',
+                        size: [
+                            300,
+                            250
+                        ]
+                    }
+                },
+                {
+                    bidder: 'ix',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        siteId: '305803',
+                        size: [
+                            300,
+                            600
+                        ]
+                    }
+                },
+                {
+                    bidder: 'openx',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        unit: '539929986',
+                        delDomain: 'streamamp-d.openx.net'
+                    }
+                },
+                {
+                    bidder: 'streamamp',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        placementId: 13075167
+                    }
+                },
+                {
+                    bidder: 'streamamp',
+                    labelAny: [
+                        '0 - 9999'
+                    ],
+                    params: {
+                        placementId: 13075176
+                    }
+                }
+            ]
+        },
+        {
+            code: '1x1-interscroller',
+            path: '/269479563/Fantasy.road.cc/1x1-interscroller',
+            mediaTypes: {
+                banner: {
+                    sizes: [
+                        [
+                            1,
+                            1
+                        ]
+                    ]
+                }
+            },
+            isSticky: false,
+            safeFrame: false,
+            outOfPage: false,
+            bids: []
+        }
+    ],
+    cmp: {
+        isEnabled: true,
+        config: {
+            'UI Layout': 'popup',
+            Language: 'en',
+            'No Option': false,
+            'Non-Consent Display Frequency': 7,
+            'Publisher Purpose IDs': [
+                1,
+                2,
+                3,
+                4,
+                5
+            ],
+            'Initial Screen Body Text Option': 1,
+            'Initial Screen Accept Button Text': 'I agree',
+            'Initial Screen Reject Button Text': 'I do not accept',
+            'Purpose Screen Body Text': 'You can set your consent preferences and determine how you want your data to be used based on the purposes below. You may set your preferences for us independently from those of third-party partners. Each purpose has a description so that you know how we and partners use your data.',
+            'Vendor Screen Body Text': 'You can set consent preferences for each individual third-party company below. Expand each company list item to see what purposes they use data for to help make your choices. In some cases, companies may disclose that they use your data without asking for your consent, based on their legitimate interests. You can click on their privacy policies for more information and to opt out.',
+            'Vendor Screen Accept All Button Text': 'Accept all',
+            'Vendor Screen Reject All Button Text': 'Reject all',
+            'Publisher Name': 'Road.cc',
+            'Publisher Logo': 'https://static.amp.services/logos/fantasy.png'
+        },
+        hasCustomStyles: false,
+        styles: {
+            ui: {},
+            link: {},
+            primaryButton: {},
+            primaryButtonHover: {},
+            isSecondaryButtonHidden: false,
+            secondaryButton: {},
+            secondaryButtonHover: {},
+            tableHeader: {},
+            tableRow: {},
+            toggleOn: {},
+            toggleOff: {},
+            additionalStyles: ''
+        }
+    },
+    breakpoints: [
+        {
+            label: '0 - 9999',
+            minWidth: 0,
+            maxWidth: 9999,
+            sizesSupported: [
+                [
+                    970,
+                    250
+                ],
+                [
+                    728,
+                    90
+                ],
+                [
+                    300,
+                    250
+                ],
+                [
+                    300,
+                    600
+                ]
+            ]
+        }
+    ]
+}
