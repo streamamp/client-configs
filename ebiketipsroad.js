177b64964a85cd3c6093aefe1996fd6ddbb13f9a
seongjaemoon <seongjae.moon.92@gmail.com>
2019-09-24 15:38:20 +0100

    display persistent consent link added, aps pub id added, renamed the file name from ebiketips to ebiketipsroad

--- ebiketipsroad.js
+++ ebiketipsroad.js
@@ -1,5 +1,6 @@
 var streamampConfig = {
     a9Enabled: false,
+    apsPubID: 'aac344f8-dc17-4ab8-b0a7-91cd349ec3b1',
     bidTimeout: 1.2,
     pbjsPriceGranularity: 'high',
     hasRefreshBids: false,
@@ -807,7 +808,8 @@ var streamampConfig = {
             'Vendor Screen Accept All Button Text': 'Accept all',
             'Vendor Screen Reject All Button Text': 'Reject all',
             'Publisher Name': 'Road.cc',
-            'Publisher Logo': 'https://static.amp.services/logos/ebiketips.png'
+            'Publisher Logo': 'https://static.amp.services/logos/ebiketips.png',
+            'Display Persistent Consent Link': false
         },
         hasCustomStyles: false,
         styles: {
