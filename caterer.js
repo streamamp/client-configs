dd37e932456e2ec1506d87510eeacc8682803461
Stephen Sabo <sabo.stephen@gmail.com>
2019-11-01 09:52:09 +0000

    caterer initial script

---
+++ caterer.js
@@ -0,0 +1,537 @@
+var streamampConfig = {
+	levelTargeting: false,
+	a9Enabled: true,
+	apsPubID: 'aac344f8-dc17-4ab8-b0a7-91cd349ec3b1',
+	bidTimeout: 1.2,
+	pbjsPriceGranularity: 'high',
+	hasRefreshBids: false,
+	hasCollapsedEmptyDivs: false,
+	publisher_id: 'TotaljobsGroupLtd/Caterer',
+	token: 'X4THqgpx1Ytwe70XiqL9yXZqbH7besUWSXjgQovgs2z5g81MVu1Y9CbTC4LshmLT',
+	currency: {
+		enabled: true,
+		value: 'GPB'
+	},
+	namespace: 'streamamp',
+	globalKeyValues: [],
+	keyValues: {},
+	adUnits: [
+		{
+			code: 'header-adbanner',
+			path: '/53560144/CR-Results-Leaderboard-728x90',
+			mediaTypes: {
+				banner: {
+					sizes: [
+						[
+							728,
+							90
+						]
+					]
+				}
+			},
+			isSticky: false,
+			safeFrame: false,
+			outOfPage: false,
+			bids: [
+				{
+					bidder: 'districtmDMX',
+					labelAny: [
+						'1810-9999',
+						'1400-1809',
+						'1000-1399',
+						'768-999'
+					],
+					params: {
+						dmxid: 355026,
+						memberid: 100615
+					}
+				},
+				{
+					bidder: 'emx_digital',
+					labelAny: [
+						'1810-9999',
+						'1400-1809',
+						'1000-1399',
+						'768-999'
+					],
+					params: {
+						tagid: '40476'
+					}
+				},
+				{
+					bidder: 'improvedigital',
+					labelAny: [
+						'1810-9999',
+						'1400-1809',
+						'1000-1399',
+						'768-999'
+					],
+					params: {
+						placementId: 1204367
+					}
+				},
+				{
+					bidder: 'ix',
+					labelAny: [
+						'1810-9999',
+						'1400-1809',
+						'1000-1399',
+						'768-999'
+					],
+					params: {
+						siteId: '185284',
+						size: [
+							728,
+							90
+						]
+					}
+				},
+				{
+					bidder: 'openx',
+					labelAny: [
+						'1810-9999',
+						'1400-1809',
+						'1000-1399',
+						'768-999'
+					],
+					params: {
+						unit: '538815343',
+						delDomain: 'streamamp-d.openx.net'
+					}
+				},
+				{
+					bidder: 'pubmatic',
+					labelAny: [
+						'1810-9999',
+						'1400-1809',
+						'1000-1399',
+						'768-999'
+					],
+					params: {
+						publisherId: '873487',
+						adSlot: 'TJ_Caterer_Unit1_728x90'
+					}
+				},
+				{
+					bidder: 'streamamp',
+					labelAny: [
+						'1810-9999',
+						'1400-1809',
+						'1000-1399',
+						'768-999'
+					],
+					params: {
+						placementId: 9734115
+					}
+				}
+			],
+			breakpoints: {
+				'1810-9999': [
+					[
+						728,
+						90
+					]
+				],
+				'1400-1809': [
+					[
+						728,
+						90
+					]
+				],
+				'1000-1399': [
+					[
+						728,
+						90
+					]
+				],
+				'768-999': [
+					[
+						728,
+						90
+					]
+				]
+			}
+		},
+		{
+			code: 'skyscraper',
+			path: '/53560144/CR-Results-Half-Page-300x600',
+			mediaTypes: {
+				banner: {
+					sizes: [
+						[
+							160,
+							600
+						],
+						[
+							300,
+							600
+						]
+					]
+				}
+			},
+			isSticky: false,
+			safeFrame: false,
+			outOfPage: false,
+			bids: [
+				{
+					bidder: 'districtmDMX',
+					labelAny: [
+						'1810-9999',
+						'1400-1809'
+					],
+					params: {
+						dmxid: 355027,
+						memberid: 100615
+					}
+				},
+				{
+					bidder: 'districtmDMX',
+					labelAny: [
+						'1810-9999'
+					],
+					params: {
+						dmxid: 355028,
+						memberid: 100615
+					}
+				},
+				{
+					bidder: 'emx_digital',
+					labelAny: [
+						'1810-9999',
+						'1400-1809'
+					],
+					params: {
+						tagid: '67073'
+					}
+				},
+				{
+					bidder: 'emx_digital',
+					labelAny: [
+						'1810-9999'
+					],
+					params: {
+						tagid: '67074'
+					}
+				},
+				{
+					bidder: 'improvedigital',
+					labelAny: [
+						'1810-9999',
+						'1400-1809'
+					],
+					params: {
+						placementId: 1204368
+					}
+				},
+				{
+					bidder: 'improvedigital',
+					labelAny: [
+						'1810-9999'
+					],
+					params: {
+						placementId: 1204368
+					}
+				},
+				{
+					bidder: 'ix',
+					labelAny: [
+						'1810-9999',
+						'1400-1809'
+					],
+					params: {
+						siteId: '185389',
+						size: [
+							160,
+							600
+						]
+					}
+				},
+				{
+					bidder: 'ix',
+					labelAny: [
+						'1810-9999'
+					],
+					params: {
+						siteId: '187944',
+						size: [
+							300,
+							600
+						]
+					}
+				},
+				{
+					bidder: 'openx',
+					labelAny: [
+						'1810-9999',
+						'1400-1809'
+					],
+					params: {
+						unit: '538815344',
+						delDomain: 'streamamp-d.openx.net'
+					}
+				},
+				{
+					bidder: 'openx',
+					labelAny: [
+						'1810-9999'
+					],
+					params: {
+						unit: '67073',
+						delDomain: 'streamamp-d.openx.net'
+					}
+				},
+				{
+					bidder: 'pubmatic',
+					labelAny: [
+						'1810-9999',
+						'1400-1809'
+					],
+					params: {
+						publisherId: '873489',
+						adSlot: 'TJ_Caterer_Unit2_160x600'
+					}
+				},
+				{
+					bidder: 'pubmatic',
+					labelAny: [
+						'1810-9999'
+					],
+					params: {
+						publisherId: '2201815',
+						adSlot: 'TJ_Caterer_Unit2_300x600'
+					}
+				},
+				{
+					bidder: 'streamamp',
+					labelAny: [
+						'1810-9999',
+						'1400-1809'
+					],
+					params: {
+						placementId: 9734129
+					}
+				},
+				{
+					bidder: 'streamamp',
+					labelAny: [
+						'1810-9999'
+					],
+					params: {
+						placementId: 9876837
+					}
+				}
+			],
+			breakpoints: {
+				'1810-9999': [
+					[
+						160,
+						600
+					],
+					[
+						300,
+						600
+					]
+				],
+				'1400-1809': [
+					[
+						160,
+						600
+					]
+				]
+			}
+		},
+		{
+			code: 'mobile-adbanner',
+			path: '/53560144/CR-Mobile-320x50',
+			mediaTypes: {
+				banner: {
+					sizes: [
+						[
+							320,
+							50
+						]
+					]
+				}
+			},
+			isSticky: false,
+			safeFrame: false,
+			outOfPage: false,
+			bids: [
+				{
+					bidder: 'districtmDMX',
+					labelAny: [
+						'0-349',
+						'350-767'
+					],
+					params: {
+						dmxid: 355029,
+						memberid: 100615
+					}
+				},
+				{
+					bidder: 'emx_digital',
+					labelAny: [
+						'0-349',
+						'350-767'
+					],
+					params: {
+						tagid: '67076'
+					}
+				},
+				{
+					bidder: 'improvedigital',
+					labelAny: [
+						'0-349',
+						'350-767'
+					],
+					params: {
+						placementId: 1204370
+					}
+				},
+				{
+					bidder: 'ix',
+					labelAny: [
+						'0-349',
+						'350-767'
+					],
+					params: {
+						siteId: '185394',
+						size: [
+							320,
+							50
+						]
+					}
+				},
+				{
+					bidder: 'openx',
+					labelAny: [
+						'0-349',
+						'350-767'
+					],
+					params: {
+						unit: '538815346',
+						delDomain: 'streamamp-d.openx.net'
+					}
+				},
+				{
+					bidder: 'pubmatic',
+					labelAny: [
+						'0-349',
+						'350-767'
+					],
+					params: {
+						publisherId: '873496',
+						adSlot: 'TJ_Caterer_Unit4_320x50'
+					}
+				},
+				{
+					bidder: 'streamamp',
+					labelAny: [
+						'0-349',
+						'350-767'
+					],
+					params: {
+						placementId: 9734140
+					}
+				}
+			],
+			breakpoints: {
+				'0-349': [
+					[
+						320,
+						50
+					]
+				],
+				'350-767': [
+					[
+						320,
+						50
+					]
+				]
+			}
+		}
+	],
+	cmp: {
+		isEnabled: false
+	},
+	breakpoints: [
+		{
+			label: '1810-9999',
+			minWidth: 1810,
+			maxWidth: 9999,
+			sizesSupported: [
+				[
+					728,
+					90
+				],
+				[
+					160,
+					600
+				],
+				[
+					300,
+					600
+				]
+			]
+		},
+		{
+			label: '1400-1809',
+			minWidth: 1400,
+			maxWidth: 1809,
+			sizesSupported: [
+				[
+					728,
+					90
+				],
+				[
+					160,
+					600
+				]
+			]
+		},
+		{
+			label: '1000-1399',
+			minWidth: 1000,
+			maxWidth: 1399,
+			sizesSupported: [
+				[
+					728,
+					90
+				]
+			]
+		},
+		{
+			label: '768-999',
+			minWidth: 768,
+			maxWidth: 999,
+			sizesSupported: [
+				[
+					728,
+					90
+				]
+			]
+		},
+		{
+			label: '350-767',
+			minWidth: 350,
+			maxWidth: 767,
+			sizesSupported: [
+				[
+					320,
+					50
+				]
+			]
+		},
+		{
+			label: '0-349',
+			minWidth: 0,
+			maxWidth: 349,
+			sizesSupported: [
+				[
+					320,
+					50
+				]
+			]
+		}
+	]
+};
