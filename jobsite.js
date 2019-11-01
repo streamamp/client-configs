4123bf9d47290997bef4519d3650016d7b2021a1
Wesley <hallwesley@outlook.com>
2019-11-01 13:58:40 +0000

    jobsite initial config

---
+++ jobsite.js
@@ -0,0 +1,469 @@
+var streamampConfig = {
+    levelTargeting: false,
+    a9Enabled: true,
+    apsPubID: 'aac344f8-dc17-4ab8-b0a7-91cd349ec3b1',
+    bidTimeout: 1.2,
+    pbjsPriceGranularity: 'high',
+    hasRefreshBids: false,
+    refreshBidsTimeout: 90,
+    hasCollapsedEmptyDivs: true,
+    publisher_id: 'TotaljobsGroupLtd/Jobsite',
+    token: 'hWu9qFV2SkIMN3Eak2g6NuzXNgR6MBVCdnZj2vI3TBNzTKk5rndnWmbyusUrjh2H',
+    currency: {
+        enabled: true,
+        value: 'GBP'
+    },
+    namespace: 'streamamp',
+    globalKeyValues: [],
+    keyValues: {},
+    adUnits: [
+        {
+            code: 'Results Leaderboard',
+            path: '/53560144/JS-Results-Leaderboard-728x90',
+            mediaTypes: {
+                banner: {
+                    sizes: [
+                        [
+                            728,
+                            90
+                        ]
+                    ]
+                }
+            },
+            isSticky: false,
+            safeFrame: false,
+            outOfPage: false,
+            bids: [
+                {
+                    bidder: 'districtmDMX',
+                    labelAny: [
+                        '1400-9999',
+                        '1000-1399',
+                        '768-999'
+                    ],
+                    params: {
+                        dmxid: 354984,
+                        memberid: 100615
+                    }
+                },
+                {
+                    bidder: 'emx_digital',
+                    labelAny: [
+                        '1400-9999',
+                        '1000-1399',
+                        '768-999'
+                    ],
+                    params: {
+                        tagid: '41009'
+                    }
+                },
+                {
+                    bidder: 'improvedigital',
+                    labelAny: [
+                        '1400-9999',
+                        '1000-1399',
+                        '768-999'
+                    ],
+                    params: {
+                        placementId: 1204379
+                    }
+                },
+                {
+                    bidder: 'ix',
+                    labelAny: [
+                        '1400-9999',
+                        '1000-1399',
+                        '768-999'
+                    ],
+                    params: {
+                        siteId: '309660',
+                        size: [
+                            728,
+                            90
+                        ]
+                    }
+                },
+                {
+                    bidder: 'openx',
+                    labelAny: [
+                        '1400-9999',
+                        '1000-1399',
+                        '768-999'
+                    ],
+                    params: {
+                        unit: '540784063',
+                        delDomain: 'streamamp-d.openx.net'
+                    }
+                },
+                {
+                    bidder: 'streamamp',
+                    labelAny: [
+                        '1400-9999',
+                        '1000-1399',
+                        '768-999'
+                    ],
+                    params: {
+                        placementId: 12953090
+                    }
+                }
+            ],
+            breakpoints: {
+                '1400-9999': [
+                    [
+                        728,
+                        90
+                    ]
+                ],
+                '1000-1399': [
+                    [
+                        728,
+                        90
+                    ]
+                ],
+                '768-999': [
+                    [
+                        728,
+                        90
+                    ]
+                ]
+            }
+        },
+        {
+            code: 'Results Half Page',
+            path: '/53560144/JS-Results-Half-Page-300x600',
+            mediaTypes: {
+                banner: {
+                    sizes: [
+                        [
+                            160,
+                            600
+                        ]
+                    ]
+                }
+            },
+            isSticky: false,
+            safeFrame: false,
+            outOfPage: false,
+            bids: [
+                {
+                    bidder: 'districtmDMX',
+                    labelAny: [
+                        '1400-9999'
+                    ],
+                    params: {
+                        dmxid: 354985,
+                        memberid: 100615
+                    }
+                },
+                {
+                    bidder: 'emx_digital',
+                    labelAny: [
+                        '1400-9999'
+                    ],
+                    params: {
+                        tagid: '41010'
+                    }
+                },
+                {
+                    bidder: 'improvedigital',
+                    labelAny: [
+                        '1400-9999'
+                    ],
+                    params: {
+                        placementId: 1204380
+                    }
+                },
+                {
+                    bidder: 'ix',
+                    labelAny: [
+                        '1400-9999'
+                    ],
+                    params: {
+                        siteId: '309658',
+                        size: [
+                            160,
+                            600
+                        ]
+                    }
+                },
+                {
+                    bidder: 'openx',
+                    labelAny: [
+                        '1400-9999'
+                    ],
+                    params: {
+                        unit: '540784064',
+                        delDomain: 'streamamp-d.openx.net'
+                    }
+                },
+                {
+                    bidder: 'streamamp',
+                    labelAny: [
+                        '1400-9999'
+                    ],
+                    params: {
+                        placementId: 12953096
+                    }
+                }
+            ],
+            breakpoints: {
+                '1400-9999': [
+                    [
+                        160,
+                        600
+                    ]
+                ]
+            }
+        },
+        {
+            code: 'Results Skyscraper',
+            path: '/53560144/JS-Results-Skyscraper-160x600',
+            mediaTypes: {
+                banner: {
+                    sizes: [
+                        [
+                            160,
+                            600
+                        ]
+                    ]
+                }
+            },
+            isSticky: false,
+            safeFrame: false,
+            outOfPage: false,
+            bids: [
+                {
+                    bidder: 'districtmDMX',
+                    labelAny: [
+                        '1400-9999'
+                    ],
+                    params: {
+                        dmxid: 354988,
+                        memberid: 100615
+                    }
+                },
+                {
+                    bidder: 'emx_digital',
+                    labelAny: [
+                        '1400-9999'
+                    ],
+                    params: {
+                        tagid: '41012'
+                    }
+                },
+                {
+                    bidder: 'improvedigital',
+                    labelAny: [
+                        '1400-9999'
+                    ],
+                    params: {
+                        placementId: 1204382
+                    }
+                },
+                {
+                    bidder: 'ix',
+                    labelAny: [
+                        '1400-9999'
+                    ],
+                    params: {
+                        siteId: '309663',
+                        size: [
+                            160,
+                            600
+                        ]
+                    }
+                },
+                {
+                    bidder: 'openx',
+                    labelAny: [
+                        '1400-9999'
+                    ],
+                    params: {
+                        unit: '540784066',
+                        delDomain: 'streamamp-d.openx.net'
+                    }
+                },
+                {
+                    bidder: 'streamamp',
+                    labelAny: [
+                        '1400-9999'
+                    ],
+                    params: {
+                        placementId: 12953105
+                    }
+                }
+            ],
+            breakpoints: {
+                '1400-9999': [
+                    [
+                        160,
+                        600
+                    ]
+                ]
+            }
+        },
+        {
+            code: 'Mobile',
+            path: '/53560144/JS-Mobile-320x50',
+            mediaTypes: {
+                banner: {
+                    sizes: [
+                        [
+                            320,
+                            50
+                        ]
+                    ]
+                }
+            },
+            isSticky: false,
+            safeFrame: false,
+            outOfPage: false,
+            bids: [
+                {
+                    bidder: 'districtmDMX',
+                    labelAny: [
+                        '350-767',
+                        '0-349'
+                    ],
+                    params: {
+                        dmxid: 354987,
+                        memberid: 100615
+                    }
+                },
+                {
+                    bidder: 'emx_digital',
+                    labelAny: [
+                        '350-767',
+                        '0-349'
+                    ],
+                    params: {
+                        tagid: '41011'
+                    }
+                },
+                {
+                    bidder: 'improvedigital',
+                    labelAny: [
+                        '350-767',
+                        '0-349'
+                    ],
+                    params: {
+                        placementId: 1204381
+                    }
+                },
+                {
+                    bidder: 'ix',
+                    labelAny: [
+                        '350-767',
+                        '0-349'
+                    ],
+                    params: {
+                        siteId: '309656',
+                        size: [
+                            320,
+                            50
+                        ]
+                    }
+                },
+                {
+                    bidder: 'openx',
+                    labelAny: [
+                        '350-767',
+                        '0-349'
+                    ],
+                    params: {
+                        unit: '540784065',
+                        delDomain: 'streamamp-d.openx.net'
+                    }
+                },
+                {
+                    bidder: 'streamamp',
+                    labelAny: [
+                        '350-767',
+                        '0-349'
+                    ],
+                    params: {
+                        placementId: 12953101
+                    }
+                }
+            ],
+            breakpoints: {
+                '350-767': [
+                    [
+                        320,
+                        50
+                    ]
+                ],
+                '0-349': [
+                    [
+                        320,
+                        50
+                    ]
+                ]
+            }
+        }
+    ],
+    cmp: {
+        isEnabled: false
+    },
+    breakpoints: [
+        {
+            label: '1400-9999',
+            minWidth: 1400,
+            maxWidth: 9999,
+            sizesSupported: [
+                [
+                    728,
+                    90
+                ],
+                [
+                    160,
+                    600
+                ]
+            ]
+        },
+        {
+            label: '1000-1399',
+            minWidth: 1000,
+            maxWidth: 1399,
+            sizesSupported: [
+                [
+                    728,
+                    90
+                ]
+            ]
+        },
+        {
+            label: '768-999',
+            minWidth: 768,
+            maxWidth: 999,
+            sizesSupported: [
+                [
+                    728,
+                    90
+                ]
+            ]
+        },
+        {
+            label: '350-767',
+            minWidth: 350,
+            maxWidth: 767,
+            sizesSupported: [
+                [
+                    320,
+                    50
+                ]
+            ]
+        },
+        {
+            label: '0-349',
+            minWidth: 0,
+            maxWidth: 349,
+            sizesSupported: [
+                [
+                    320,
+                    50
+                ]
+            ]
+        }
+    ]
+}
