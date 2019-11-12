671235b62e3a0a9fb7a67a0d60c3e06ca75598fa
Stephen Sabo <sabo.stephen@gmail.com>
2019-11-12 11:43:54 +0000

    remove persitent consent button

--- accountingweb.js
+++ accountingweb.js
@@ -980,7 +980,7 @@ var streamampConfig = {
             'Vendor Screen Accept All Button Text': 'Accept all',
             'Vendor Screen Reject All Button Text': 'Reject all',
             'Publisher Name': 'Accounting Web UK',
-            'Publisher Logo': 'https://static.amp.services/logos/siftlogo.png'
+            'Publisher Logo': 'https://static.amp.services/logos/siftlogo.png',
             'Display Persistent Consent Link': false
         },
         hasCustomStyles: false,
