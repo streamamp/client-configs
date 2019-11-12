384466b3c7e1cc8e36596cc9bb4f18a81e35c4df
wesley-hall <hallwesley@outlook.com>
2019-11-12 14:43:44 +0000

    refresh only on unit 'HRZ_Sticky'

--- hrzone.js
+++ hrzone.js
@@ -5,6 +5,7 @@ var streamampConfig = {
     bidTimeout: 1.2,
     pbjsPriceGranularity: 'high',
     hasRefreshBids: true,
+    adUnitsToRefresh: 'HRZ_Sticky',
     minRefreshTime: 40,
     maxRefreshTime: 60,
     hasCollapsedEmptyDivs: true,
