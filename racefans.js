93a519c1c86486af198036c6ab3dc38e0786b2bf
wesley-hall <hallwesley@outlook.com>
2019-11-14 15:19:43 +0000

    gpt single request disabled

--- racefans.js
+++ racefans.js
@@ -1,5 +1,5 @@
 var streamampConfig = {
-	gptSingleRequestEnabled: true,
+	gptSingleRequestEnabled: false,
 	levelTargeting: false,
 	a9Enabled: true,
 	apsPubID: '16268e26-dabe-4bf4-a28f-b8f4ee192ed3',
