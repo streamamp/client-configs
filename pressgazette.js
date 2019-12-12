0057de5fe91482a5f28ef365084ed51134346157
wesley-hall <hallwesley@outlook.com>
2019-12-12 12:38:19 +0000

    semicolon" "

--- pressgazette.js
+++ pressgazette.js
@@ -6,7 +6,7 @@ var streamampConfig = {
     bidTimeout: 1.2,
     pbjsPriceGranularity: 'high',
     hasRefreshBids: true,
-    adUnitsToRefresh:['Unit1', 'Unit2', 'Unit3', 'Unit4', 'Unit5', 'Unit6', 'Unit7'],
+    adUnitsToRefresh: ['Unit1', 'Unit2', 'Unit3', 'Unit4', 'Unit5', 'Unit6', 'Unit7'],
     maxRefreshTime: 120,
     minRefreshTime: 120,
     hasCollapsedEmptyDivs: true,
@@ -2157,4 +2157,4 @@ var streamampConfig = {
             ]
         }
     ]
-}
+};
