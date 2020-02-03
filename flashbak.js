d6b0980bfe54fdc4768e9b68420a940b6c98b082
Stephen Sabo <sabo.stephen@gmail.com>
2020-02-03 14:32:53 +0000

    Flashbak initial script

---
+++ flashbak.js
@@ -0,0 +1,757 @@
+var streamampConfig = {
+	levelTargeting: false,
+	a9Enabled: false,
+	apsPubID: 'aac344f8-dc17-4ab8-b0a7-91cd349ec3b1',
+	bidTimeout: 1.2,
+	pbjsPriceGranularity: 'high',
+	hasRefreshBids: true,
+	minRefreshTime: 90,
+	maxRefreshTime: 120,
+	hasCollapsedEmptyDivs: true,
+	publisher_id: 'PaulSorene/Flashbak',
+	token: '',
+	currency: {
+		enabled: true,
+		value: 'GBP'
+	},
+	namespace: 'streamamp',
+	globalKeyValues: [],
+	keyValues: {},
+	adUnits: [
+		{
+			code: 'Unit1',
+			path: '/109048388/Flashbak/Unit1',
+			mediaTypes: {
+				banner: {
+					sizes: [
+						[
+							320,
+							50
+						],
+						[
+							728,
+							90
+						],
+						[
+							468,
+							60
+						],
+						[
+							970,
+							90
+						]
+					]
+				}
+			},
+			isSticky: false,
+			safeFrame: false,
+			outOfPage: false,
+			bids: [],
+			breakpoints: {
+				'971 - 9999': [
+					[
+						728,
+						90
+					],
+					[
+						970,
+						90
+					]
+				],
+				'729 - 970': [
+					[
+						728,
+						90
+					],
+					[
+						468,
+						60
+					]
+				],
+				'0 - 728': [
+					[
+						320,
+						50
+					]
+				]
+			}
+		},
+		{
+			code: 'Unit2',
+			path: '/109048388/Flashbak/Unit2',
+			mediaTypes: {
+				banner: {
+					sizes: [
+						[
+							320,
+							50
+						],
+						[
+							728,
+							90
+						],
+						[
+							468,
+							60
+						],
+						[
+							970,
+							90
+						],
+						[
+							970,
+							250
+						]
+					]
+				}
+			},
+			isSticky: false,
+			safeFrame: false,
+			outOfPage: false,
+			bids: [],
+			breakpoints: {
+				'971 - 9999': [
+					[
+						728,
+						90
+					],
+					[
+						970,
+						90
+					],
+					[
+						970,
+						250
+					]
+				],
+				'729 - 970': [
+					[
+						728,
+						90
+					],
+					[
+						468,
+						60
+					]
+				],
+				'0 - 728': [
+					[
+						320,
+						50
+					]
+				]
+			}
+		},
+		{
+			code: 'Unit3',
+			path: '/109048388/Flashbak/Unit3',
+			mediaTypes: {
+				banner: {
+					sizes: [
+						[
+							320,
+							50
+						],
+						[
+							728,
+							90
+						],
+						[
+							468,
+							60
+						],
+						[
+							970,
+							90
+						],
+						[
+							970,
+							250
+						]
+					]
+				}
+			},
+			isSticky: false,
+			safeFrame: false,
+			outOfPage: false,
+			bids: [],
+			breakpoints: {
+				'971 - 9999': [
+					[
+						728,
+						90
+					],
+					[
+						970,
+						90
+					],
+					[
+						970,
+						250
+					]
+				],
+				'729 - 970': [
+					[
+						728,
+						90
+					],
+					[
+						468,
+						60
+					]
+				],
+				'0 - 728': [
+					[
+						320,
+						50
+					]
+				]
+			}
+		},
+		{
+			code: 'Unit4',
+			path: '/109048388/Flashbak/Unit4',
+			mediaTypes: {
+				banner: {
+					sizes: [
+						[
+							300,
+							250
+						],
+						[
+							728,
+							90
+						],
+						[
+							468,
+							60
+						],
+						[
+							970,
+							90
+						],
+						[
+							970,
+							250
+						]
+					]
+				}
+			},
+			isSticky: false,
+			safeFrame: false,
+			outOfPage: false,
+			bids: [],
+			breakpoints: {
+				'971 - 9999': [
+					[
+						728,
+						90
+					],
+					[
+						970,
+						90
+					],
+					[
+						970,
+						250
+					]
+				],
+				'729 - 970': [
+					[
+						728,
+						90
+					],
+					[
+						468,
+						60
+					]
+				],
+				'0 - 728': [
+					[
+						300,
+						250
+					]
+				]
+			}
+		},
+		{
+			code: 'Unit5',
+			path: '/109048388/Flashbak/Unit5',
+			mediaTypes: {
+				banner: {
+					sizes: [
+						[
+							300,
+							250
+						],
+						[
+							728,
+							90
+						],
+						[
+							468,
+							60
+						],
+						[
+							970,
+							90
+						],
+						[
+							970,
+							250
+						]
+					]
+				}
+			},
+			isSticky: false,
+			safeFrame: false,
+			outOfPage: false,
+			bids: [],
+			breakpoints: {
+				'971 - 9999': [
+					[
+						728,
+						90
+					],
+					[
+						970,
+						90
+					],
+					[
+						970,
+						250
+					]
+				],
+				'729 - 970': [
+					[
+						728,
+						90
+					],
+					[
+						468,
+						60
+					]
+				],
+				'0 - 728': [
+					[
+						300,
+						250
+					]
+				]
+			}
+		},
+		{
+			code: 'Unit6',
+			path: '/109048388/Flashbak/Unit6',
+			mediaTypes: {
+				banner: {
+					sizes: [
+						[
+							300,
+							250
+						],
+						[
+							728,
+							90
+						],
+						[
+							468,
+							60
+						],
+						[
+							970,
+							90
+						],
+						[
+							970,
+							250
+						]
+					]
+				}
+			},
+			isSticky: false,
+			safeFrame: false,
+			outOfPage: false,
+			bids: [],
+			breakpoints: {
+				'971 - 9999': [
+					[
+						728,
+						90
+					],
+					[
+						970,
+						90
+					],
+					[
+						970,
+						250
+					]
+				],
+				'729 - 970': [
+					[
+						728,
+						90
+					],
+					[
+						468,
+						60
+					]
+				],
+				'0 - 728': [
+					[
+						300,
+						250
+					]
+				]
+			}
+		},
+		{
+			code: 'Unit7',
+			path: '/109048388/Flashbak/Unit7',
+			mediaTypes: {
+				banner: {
+					sizes: [
+						[
+							300,
+							250
+						],
+						[
+							728,
+							90
+						],
+						[
+							468,
+							60
+						],
+						[
+							970,
+							90
+						],
+						[
+							970,
+							250
+						]
+					]
+				}
+			},
+			isSticky: false,
+			safeFrame: false,
+			outOfPage: false,
+			bids: [],
+			breakpoints: {
+				'971 - 9999': [
+					[
+						728,
+						90
+					],
+					[
+						970,
+						90
+					],
+					[
+						970,
+						250
+					]
+				],
+				'729 - 970': [
+					[
+						728,
+						90
+					],
+					[
+						468,
+						60
+					]
+				],
+				'0 - 728': [
+					[
+						300,
+						250
+					]
+				]
+			}
+		},
+		{
+			code: 'Unit8',
+			path: '/109048388/Flashbak/Unit8',
+			mediaTypes: {
+				banner: {
+					sizes: [
+						[
+							300,
+							250
+						],
+						[
+							300,
+							600
+						]
+					]
+				}
+			},
+			isSticky: false,
+			safeFrame: false,
+			outOfPage: false,
+			bids: [],
+			breakpoints: {
+				'0 - 728': [
+					[
+						300,
+						250
+					],
+					[
+						300,
+						600
+					]
+				]
+			}
+		},
+		{
+			code: 'Unit9',
+			path: '/109048388/Flashbak/Unit9',
+			mediaTypes: {
+				banner: {
+					sizes: [
+						[
+							300,
+							250
+						],
+						[
+							300,
+							600
+						]
+					]
+				}
+			},
+			isSticky: false,
+			safeFrame: false,
+			outOfPage: false,
+			bids: [],
+			breakpoints: {
+				'0 - 728': [
+					[
+						300,
+						250
+					],
+					[
+						300,
+						600
+					]
+				]
+			}
+		},
+		{
+			code: 'Unit10',
+			path: '/109048388/Flashbak/Unit10',
+			mediaTypes: {
+				banner: {
+					sizes: [
+						[
+							300,
+							250
+						],
+						[
+							300,
+							600
+						]
+					]
+				}
+			},
+			isSticky: false,
+			safeFrame: false,
+			outOfPage: false,
+			bids: [],
+			breakpoints: {
+				'0 - 728': [
+					[
+						300,
+						250
+					],
+					[
+						300,
+						600
+					]
+				]
+			}
+		},
+		{
+			code: 'Sticky',
+			path: '/109048388/Flashbak/Sticky',
+			mediaTypes: {
+				banner: {
+					sizes: [
+						[
+							320,
+							50
+						],
+						[
+							728,
+							90
+						]
+					]
+				}
+			},
+			isSticky: true,
+			safeFrame: false,
+			outOfPage: false,
+			bids: [],
+			breakpoints: {
+				'971 - 9999': [
+					[
+						728,
+						90
+					]
+				],
+				'729 - 970': [
+					[
+						728,
+						90
+					]
+				],
+				'0 - 728': [
+					[
+						320,
+						50
+					]
+				]
+			}
+		},
+		{
+			code: 'Skin',
+			path: '/109048388/Flashbak/Skin',
+			mediaTypes: {
+				banner: {
+					sizes: [
+						[
+							1,
+							1
+						]
+					]
+				}
+			},
+			isSticky: false,
+			safeFrame: false,
+			outOfPage: false,
+			bids: [],
+			breakpoints: {
+				'971 - 9999': [
+					[
+						1,
+						1
+					]
+				],
+				'729 - 970': [
+					[
+						1,
+						1
+					]
+				],
+				'0 - 728': [
+					[
+						1,
+						1
+					]
+				]
+			}
+		},
+	],
+	cmp: {
+		isEnabled: true,
+		config: {
+			'UI Layout': 'popup',
+			'Language': 'en',
+			'No Option': false,
+			'Non-Consent Display Frequency': 7,
+			'Initial Screen Body Text Option': 1,
+			'Vendor Screen Accept All Button Text': 'Accept all',
+			'Vendor Screen Reject All Button Text': 'Reject all',
+			'Publisher Name': 'Flashbak',
+		},
+		hasCustomStyles: false,
+		styles: {
+			ui: {},
+			link: {},
+			primaryButton: {},
+			primaryButtonHover: {},
+			isSecondaryButtonHidden: false,
+			secondaryButton: {},
+			secondaryButtonHover: {},
+			tableHeader: {},
+			tableRow: {},
+			toggleOn: {},
+			toggleOff: {},
+			additionalStyles: ''
+		}
+	},
+	breakpoints: [
+		{
+			label: '971 - 9999',
+			minWidth: 971,
+			maxWidth: 9999,
+			sizesSupported: [
+				[
+					300,
+					250
+				],
+				[
+					300,
+					600
+				],
+				[
+					970,
+					250
+				],
+				[
+					970,
+					90
+				],
+				[
+					728,
+					90
+				],
+				[
+					1,
+					1
+				]
+			]
+		},
+		{
+			label: '729 - 970',
+			minWidth: 729,
+			maxWidth: 970,
+			sizesSupported: [
+				[
+					728,
+					90
+				],
+				[
+					468,
+					60
+				],
+				[
+					1,
+					1
+				]
+			]
+		},
+		{
+			label: '0 - 728',
+			minWidth: 0,
+			maxWidth: 728,
+			sizesSupported: [
+				[
+					300,
+					250
+				],
+				[
+					320,
+					50
+				],
+				[
+					1,
+					1
+				]
+			]
+		}
+	]
+};
